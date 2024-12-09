aa2bb3ef8ca15dc2edad76df099b2e61295d81f5
Your Name <subasiarhan3@gmail.com>
2024-12-09 19:04:32 +0300

    commit

---
+++ blueprint.config.ts
@@ -0,0 +1,22 @@
+import { Blueprint } from '@ton-community/blueprint';
+
+export const blueprint: Blueprint = {
+  contracts: {
+    vault: {
+      path: 'contracts/vault/Vault.fc',
+      output: 'build/vault',
+    },
+    baseStrategy: {
+      path: 'contracts/strategy/BaseStrategy.fc',
+      output: 'build/baseStrategy',
+    },
+    dedustStrategy: {
+      path: 'contracts/strategy/DeDustStrategy.fc',
+      output: 'build/dedustStrategy',
+    },
+    stonStrategy: {
+      path: 'contracts/strategy/StonStrategy.fc',
+      output: 'build/stonStrategy',
+    }
+  },
+};
